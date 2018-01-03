71ecf219b40febdba3686ad01a2aaf0c7f0cb979
Peter Juhasz <jpeter82@gmail.com>
2018-01-03 13:53:38 +0100

    Add delivery controller

--- src/main/java/com/peanuts/delivery/controller/DeliveryController.java
+++ src/main/java/com/peanuts/delivery/controller/DeliveryController.java
@@ -1,4 +1,33 @@
 package com.peanuts.delivery.controller;
 
+
+import com.peanuts.delivery.dao.PostapontDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
 public class DeliveryController {
+
+    private PostapontDao postapontDao;
+
+    public DeliveryController(PostapontDao postapontDao) {
+        this.postapontDao = postapontDao;
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.POST)
+    public String queryDeliveryOptions(Model model, HttpSession session) {
+        String jsonResponse = null;
+        return jsonResponse;
+    }
+
+    @RequestMapping(value = "/order", method = RequestMethod.POST)
+    public String saveOrder(Model model, HttpSession session) {
+        String jsonResponse = null;
+        return jsonResponse;
+    }
+
 }
