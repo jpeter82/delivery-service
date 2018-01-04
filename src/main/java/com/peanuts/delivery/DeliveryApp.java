8cec743672a2683fcb7877b97c502be0abd393cf
Peter Juhasz <jpeter82@gmail.com>
2018-01-04 18:39:06 +0100

    Rename main DeliverySevice to DeliveryApp

--- src/main/java/com/peanuts/delivery/DeliveryApp.java
+++ src/main/java/com/peanuts/delivery/DeliveryApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryService {
+public class DeliveryApp {
     public static void main(String[] args) {
-        SpringApplication.run(DeliveryService.class, args);
+        SpringApplication.run(DeliveryApp.class, args);
     }
 }
